4f51907f0f4965833e88a0d63bf93db58e778438
mujin <chenjingranoop@163.com>
2018-07-20 13:23:54 +0800

    init

---
+++ src/main/java/org/mujin/springboot/SpringbootTrainingApplication.java
@@ -0,0 +1,12 @@
+package org.mujin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTrainingApplication.class, args);
+	}
+}
